82e0a01bace892921b7f498763c5e9fae1b46fdb
Sairam <jagadamsairam@gmail.com>
2020-03-10 11:46:13 +0530

    SimpleJava

---
+++ TodayDate.java
@@ -0,0 +1,14 @@
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class TodayDate{
+public static void main(Stirng[] args){
+
+LocalDate date=LocalDate.now();
+System.out.println("Today Date is "+ date);
+LocalTime time = Localtime.now();
+System.out.println("Time is "+time);
+}
+
+}
