1157f6f334c38f3c59d44b7b9904c06a399f5df8
JagadamSairam <59390594+JagadamSairam@users.noreply.github.com>
2020-03-10 12:09:36 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,8 +2,8 @@
 
 public class Sum{
 public static void main(String[] args){
-int a=args(0);
-int b=args(1);
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
 int c=a+b;
 System.out.println("Sum of "+ a + "  and "+ b + " is "+c);
 
